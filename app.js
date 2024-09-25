da5fdee9b7b64104196dddceb817fdbba364afb2
Manish kumar <manish620287@gmail.com>
2024-09-26 01:50:19 +0530

    post create

--- app.js
+++ app.js
@@ -16,6 +16,29 @@ app.get('/', (req, res)=>{
     res.render("index");
 })
 
+app.get('/login', (req, res ) => {
+    res.render("login")
+})
+
+app.get('/profile', isLoggedIn, async (req, res ) => {
+    let user = await userModel.findOne({email: req.user.email}).populate("post");;
+    
+    res.render("profile", {user})
+})
+
+app.post('/post', isLoggedIn, async (req, res ) => {
+    let user = await userModel.findOne({email: req.user.email});
+    let {content} = req.body;
+    let post = await postModel.create({
+        user: user._id,
+        content
+    });
+
+    user.post.push(post._id);
+    await user.save();
+    res.redirect("/profile");
+});
+
 app.post('/register', async (req, res)=>{
     let {name, email, username, password, age} = req.body;
 
@@ -34,13 +57,42 @@ app.post('/register', async (req, res)=>{
 
             let token = jwt.sign({email: email, userid: user._id}, "secretkey");
             res.cookie("token", token);
-            res.send("registered")
+            res.redirect("/profile")
 
         })
     })
-
-
     
 })
 
+app.post('/login', async (req, res)=> {
+    let {email, password} = req.body;
+
+    let user = await userModel.findOne({email});
+    if(!user) return res.status(500).redirect('login');
+
+    bcrypt.compare(password, user.password, (err, result) => {
+        if(result){
+            let token = jwt.sign({email: email, userid: user._id}, "secretkey");
+            res.cookie("token", token);
+            res.status(200).redirect("/profile");
+        }
+        else res.redirect('/login');
+    })
+
+});
+
+app.get('/logout', (req, res)=> {
+    res.cookie('token', "");
+    res.redirect('/login');
+})
+
+function isLoggedIn(req, res, next) {
+    if(req.cookies.token === "") res.send("you must need to login");
+    else{
+        let data = jwt.verify(req.cookies.token, "secretkey");
+            req.user = data ;
+            next();
+    }
+    
+}
 app.listen(3000);
